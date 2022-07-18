12a44a9c4316f4fe5b54dfb3ca783d6317ef4edf
Rounak <rounakguptabhaya@gmail,com>
2022-07-19 00:13:17 +0530

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+let income = 0;
+let expense = 0;
+let balance;
+function addStorage() {
+    var text = document.getElementById("text").value;
+    var amount = document.getElementById("amount").value
+    localStorage.setItem(text, amount);
+    //console.log(JSON.parse(localStorage.getItem(text,amount)));
+    var display = document.createElement('div');
+    display.setAttribute("id", "list1");
+    console.log(display);
+    document.getElementById('list').appendChild(display).innerHTML = text + "&nbsp&nbsp&nbsp&nbsp" + amount;
+    document.getElementById("list").style.color = "#fff";
+    if ((Math.sign(amount)) == 1) {
+        income = Number(income) + Number(amount);
+        document.getElementById('money-plus').textContent = "+$" + income;
+    }
+    else {
+        expense = Number(expense) + Number(amount);
+        document.getElementById('money-minus').textContent = "$" + expense;
+    }
+    
+    balance = Number(income) + Number(expense);
+    document.getElementById('balance').textContent = "$" + balance;
+}
